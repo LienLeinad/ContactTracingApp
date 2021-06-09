f281edd3ad3b362120a92ae59bbda18f7f5f8fed
LienLeinad <neil.bautista@obf.ateneo.edu>
2021-06-09 12:45:04 +0800

    made display that features the data for contact tracing forms. Fixed it in such a way that non-mall admins can only see contact tracing forms that are connected to their store.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,13 @@
 class ResultsController < ApplicationController
     def list
+        if !Current.user
+            redirect_to root_path, alert: "You must be logged in to view this page"
+        elsif Current.user.isMallAd #mallAd can see all the logs
+            @userList = User.all
+            @ctList = ContactTracingForm.all
+        else #nonMallAd can only see logs of their own store
+            @ctList = ContactTracingForm.joins(:user).where("contact_tracing_forms.user_id = ?", Current.user.id)
+        end
         
     end
 
